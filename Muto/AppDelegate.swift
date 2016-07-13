e52c5313d170b5e9f054a8d193b457ed641b1a0f
Kane Hsieh <kane.hsieh@gmail.com>
2016-07-12 19:29:10 -0700

    Initial Commit

---
+++ Muto/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Muto
+//
+//  Created by Kane Hsieh on 7/12/16.
+//  Copyright © 2016 Kane Hsieh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
